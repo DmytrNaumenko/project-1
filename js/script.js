8530f4aaf4fcccf4f83fa0b70beb9cf4d2bf891e
Dmytro Naumenko <naumenkoff07@gmail.com>
2022-08-12 13:25:39 +0300

    task2

--- js/script.js
+++ js/script.js
@@ -8,14 +8,30 @@ const personalMovieDB = {
     actors: {},
     genres: [],
     privat: false
-};
+}; 
 
-const a = prompt('Name one of the films you have watched??', ''),
-      b = prompt('How much would you rate it?', ''),
-      c = prompt('Name one of the films you have watched?', ''),
-      d = prompt('How much would you rate it?', '');
-     
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;    
+for (let i = 0; i < 2; i++) {
+    const a = prompt('Name one of the films you have watched??', ''),
+          b = prompt('How much would you rate it?', '');
+
+    if (a != null && b != null && a != '' && b != '' && a.length <50) {
+        personalMovieDB.movies[a] = b;
+        console.log('done!');
+    } else {
+        console.log('error!')
+        i--;
+    }
+         
+}
+
+if (personalMovieDB.count < 10) {
+    console.log('Watched quite a few films');
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+    console.log('You are an ordinary spectator');
+} else if (personalMovieDB.count >= 30) {
+    console.log('You are a film buff');
+} else {
+    console.log('Error');
+}
 
 console.log(personalMovieDB);
